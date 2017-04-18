fdc088172d5844a787fd06b67377a8c1da793a53
Mathias Dahlström <mathias@monsieurdahlstrom.com>
2017-04-18 13:31:04 +0800

    updated version and main js file

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import {StateMachine,States} from './state-machine'
+import {Firmware, FirmwareType} from './firmware'
+
+//
+module.exports.States = StateMachineStates
+module.exports.StateMachine = StateMachine
+//
+module.exports.Firmware = Firmware
+module.exports.FirmwareType = FirmwareType
